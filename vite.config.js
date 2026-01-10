fd7d77b275e9be4196ce55ce2945f3ebe5a8f339
MarkPagkaliwagan <info.markpagkaliwagan@gmail.com>
2026-01-10 12:32:54 +0800

    Updated something

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/PortfolioV2/', // <-- replace with your GitHub repo name
+
 })
