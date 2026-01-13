f1462013f8e297a3eb27f41afa098a1d51d6feba
MarkPagkaliwagan <info.markpagkaliwagan@gmail.com>
2026-01-13 20:15:48 +0800

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,20 +21,12 @@ export default function ProfileHeaderReplica({ darkMode, setDarkMode, setShowCon
 const [visitors, setVisitors] = useState(0);
 
 useEffect(() => {
-  const visited = localStorage.getItem('visited');
-
-  if (!visited) {
-    fetch('https://api.countapi.xyz/hit/markjeus.github.io/portfolio')
-      .then(res => res.json())
-      .then(data => setVisitors(data.value));
-
-    localStorage.setItem('visited', 'true');
-  } else {
-    fetch('https://api.countapi.xyz/get/markjeus.github.io/portfolio')
-      .then(res => res.json())
-      .then(data => setVisitors(data.value));
-  }
+  fetch('https://api.countapi.xyz/hit/markjeus.github.io/portfolio')
+    .then(res => res.json())
+    .then(data => setVisitors(data.value))
+    .catch(err => console.error('Error fetching visitor count:', err));
 }, []);
+
 
 
   // Update time every second
